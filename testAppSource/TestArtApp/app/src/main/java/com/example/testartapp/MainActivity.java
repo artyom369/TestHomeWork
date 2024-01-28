a344c042bcc06a38a6b15a1159cceed106434af3
ARTMAX <martem@phasev.com>
2024-01-28 19:28:04 +0200

    Add .apk source files

--- testAppSource/TestArtApp/app/src/main/java/com/example/testartapp/MainActivity.java
+++ testAppSource/TestArtApp/app/src/main/java/com/example/testartapp/MainActivity.java
@@ -1,20 +1,14 @@
 package com.example.testartapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.testartapp.databinding.ActivityMainBinding;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.testartapp.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -33,10 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-       // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder().build();
-      // setSupportActionBar(findViewById(R.id.toolbar));
-
 
         binding.textViewStatic.setText(R.string.text_view_static);
         binding.button2.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
         return true;
@@ -62,12 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.red) {
             findViewById(R.id.textViewActive).setBackgroundColor(getColor(R.color.red));
             return true;
@@ -84,10 +72,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-  /*  @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }*/
+
 }
